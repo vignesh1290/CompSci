80b59f3c45ab913a80d4a229e7e1661b43f59392
jokolpl <zwartvignesh@gmail.com>
2024-02-06 13:25:45 -0800

    m

--- Code/src/catdog/Class/Classroom.java
+++ Code/src/catdog/Class/Classroom.java
@@ -7,9 +7,17 @@ public class Classroom {
      public boolean taken=false;
      public int prioritylevel=0;
      public String ClassholderEvent;
-    public Classroom(ArrayList e){
+     public String name;
+    public Classroom(String name, ArrayList e){
         for(int i=0; i<e.size(); i++){
             amenities.add(e.get(i));
+            this.name=name;
+        }
+    }
+    public void amenties(){
+        System.out.print(name+" amenties: ");
+        for(int i=0; i<amenities.size(); i++){
+            System.out.print(amenities.get(i)+" ");
         }
     }
     
--- Code/src/catdog/Class/Event.java
+++ Code/src/catdog/Class/Event.java
@@ -3,17 +3,35 @@ package Class;
 import java.util.ArrayList;
 
 public class Event {
-    public static String name;
+    public String name;
     public int time;
     public int size;
-    
+    public int priority;
     public Event(String name, int time, int size){
         this.name=name;
         this.time=time;
         this.size=size;
     }
     public Event(){
-       
+        
     }
+    public void request(Classroom e){
+        if (e.taken==true && e.prioritylevel<priority){
+           e.ClassholderEvent=name;
+           e.prioritylevel=priority;
+           e.taken=true;
+           System.out.println(e.ClassholderEvent+ "now holds this room");
+        }
+        else if (e.taken==false){
+           e.ClassholderEvent=name;
+           System.out.println(e.ClassholderEvent+ " now holds this room");
+       }
+        else{
+            System.out.println(e.ClassholderEvent+ "holds this room");
+        }
+        
+      
+        
+      }
  
 }
